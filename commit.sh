61f057fb19bc251e63d4badbd369def3093fdc9d
Azi Angelo <90162379+Aziangelo@users.noreply.github.com>
2024-06-03 02:09:41 +0800

    Delete commit.sh

--- commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git add --all
-
-read -p "Enter a commit message: " commit_message
-git commit -m "$commit_message"
-
-git push https://Aziangelo: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@github.com/Aziangelo/AF-BRT.git
