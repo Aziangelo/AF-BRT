3c28c1357e4389d7b1ccc674bd5ab59461f62f71
Aziangelo <aziangelob@gmail.com>
2024-06-03 02:08:05 +0800

    Test

--- commit.sh
+++ commit.sh
@@ -5,4 +5,4 @@ git add --all
 read -p "Enter a commit message: " commit_message
 git commit -m "$commit_message"
 
-git push https://Aziangelo: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@github.com/Aziangelo/AF-TrulyDefault.git
+git push https://Aziangelo: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@github.com/Aziangelo/AF-BRT.git
